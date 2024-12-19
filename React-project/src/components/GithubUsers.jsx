839b43201a743cca97ec524bb1b3dc915fde57cb
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 16:58:38 +0100

    esercizio sul fetching con useEffect

---
+++ React-project/src/components/GithubUsers.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { GithubUsers } from "./components/GithubUsers";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Github User Search</h1>
+        <GithubUsers />
+      </div>
+    </>
+  );
+}
+
+export default App;
