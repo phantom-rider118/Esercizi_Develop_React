57cd1539178522c94ed64d0b77e69d18a5424860
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 16:37:37 +0100

    esercizio sullo styling dei componenti

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import "./App.css";
+import { CounterDisplay } from "./components/CounterDisplay";
+import { Login } from "./components/Login";
+import { Welcome } from "./components/Welcome";
 
 function App() {
   return (
     <>
+      <Welcome />
+      <hr />
+      <CounterDisplay />
+      <hr />
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
