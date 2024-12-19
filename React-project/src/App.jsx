b93d677d657db873efa23583103def6a58e72a73
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 17:09:47 +0100

    esercizio fatto a lezione timer fatto in 2 modi: 1 con useState e 1 con useRef

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import "./App.css";
+import { Timer } from "./components/Timer";
+import { Timer2 } from "./Components/Timer2";
 
 function App() {
   return (
     <>
+      <Timer />
+      <hr />
+      <Timer2 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
