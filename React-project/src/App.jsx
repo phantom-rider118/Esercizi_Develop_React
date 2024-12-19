e0f1d34c654e19e84d605ccc4746e6c83ecff100
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 16:49:12 +0100

    esercizio sulla composizione dei componenti

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import "./App.css";
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <>
+      <Container title="Titolo del mio Contenitore">
+        <p>Questo è il contenuto del mio contenitore</p>
+        <p>elemento</p>
+        <p>altro elemento</p>
+        <p>infine un ultimo elemento</p>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
