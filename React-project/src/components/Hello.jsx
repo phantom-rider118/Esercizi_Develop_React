ed8f7ef1768e337e6ae497dd4a7921a76cbf9deb
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 11:02:34 +0100

    esercizio sui componenti

---
+++ React-project/src/components/Hello.jsx
@@ -0,0 +1,10 @@
+import { Messaggio } from "./Messaggio";
+
+export function Hello(){
+    return(
+        <div>
+            <h2>Hello, World!</h2>
+            <Messaggio />
+        </div>
+    )
+}
---
+++ React-project/src/components/Messaggio.jsx
@@ -0,0 +1,3 @@
+export function Messaggio() {
+  return <p>Oggi è proprio una bella giornata</p>;
+}
