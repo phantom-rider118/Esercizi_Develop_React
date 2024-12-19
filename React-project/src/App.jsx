5253bd1944bef028a79d4a59c980ffd52a5383a7
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-19 12:33:13 +0100

    esercizio su uncontrolled forms

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import "./App.css";
+import { UncontrolledLogin } from "./components/UncontrolledLogin";
 
 function App() {
+  const handleLogin = (FormData) => {
+    console.log("Dati del Login:", FormData);
+  };
+
   return (
-    <>
-    </>
-  )
+    <div>
+      <UncontrolledLogin onLogin={handleLogin} />
+    </div>
+  );
 }
 
-export default App
+export default App;
